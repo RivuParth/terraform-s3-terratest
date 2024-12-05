deaca8b554fc03f7d3bf29775c71694c4e85c0a3
RivuParth <bhowmick.partha1999@gmail.com>
2024-12-05 23:20:27 +0530

    Add GitHub Actions final terratest djk

--- test/s3_test.go
+++ test/s3_test.go
@@ -12,13 +12,14 @@ import (
 func TestS3BucketModule(t *testing.T) {
 	t.Parallel()
 
+	// Set up Terraform options
 	terraformOptions := &terraform.Options{
 		// Path to the Terraform code
-		TerraformDir: "../s3", 
+		TerraformDir: "../s3",  // Modify the path to your Terraform directory if needed
 		// Variables for the Terraform module
 		Vars: map[string]interface{}{
-			"bucket_name":      "my-terratest-0000",  
-			"enable_versioning": true,               
+			"bucket_name":      "my-terratest-0000",  // Make sure the bucket name is unique
+			"enable_versioning": true,                 // Enable versioning on the bucket
 			"tags": map[string]string{
 				"Environment": "Test",
 			},
@@ -26,7 +27,6 @@ func TestS3BucketModule(t *testing.T) {
 		// Ensure Terraform doesn't ask for user input during apply
 		NoColor: true,
 	}
-	
 
 	// Clean up resources after the test
 	defer terraform.Destroy(t, terraformOptions)
@@ -35,20 +35,17 @@ func TestS3BucketModule(t *testing.T) {
 	terraform.InitAndApply(t, terraformOptions)
 
 	// Get the bucket ID and ARN from the Terraform output
-	bucketId := terraform.Output(t, terraformOptions, "bucket_id")
-	assert.NotEmpty(t, bucketId, "Bucket ID should not be empty")
+	bucketId := terraform.Output(t, terraformOptions, "bucket_name")
+	assert.NotEmpty(t, bucketId, "Bucket name should not be empty")
 
+	// Verify the S3 bucket ARN from Terraform output
 	bucketArn := terraform.Output(t, terraformOptions, "bucket_arn")
 	assert.Contains(t, bucketArn, "arn:aws:s3:::"+bucketId, "Bucket ARN should contain the correct bucket ID")
-
-	// Verify the bucket versioning status
-	// versioningStatus := terraform.Output(t, terraformOptions, "bucket_versioning_status")
-	// assert.Equal(t, "Enabled", versioningStatus, "Versioning should be enabled")
 
 	// Optionally, use AWS SDK to verify the S3 bucket's location
 	// Create a new AWS session
 	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String("us-east-1"),
+		Region: aws.String("us-east-1"),  // Make sure the region is set correctly
 	})
 	if err != nil {
 		t.Fatalf("Failed to create AWS session: %v", err)
@@ -57,10 +54,9 @@ func TestS3BucketModule(t *testing.T) {
 	// Create an S3 service client
 	s3Client := s3.New(sess)
 
-	// Get bucket location
+	// Get the bucket location
 	_, err = s3Client.GetBucketLocation(&s3.GetBucketLocationInput{
 		Bucket: aws.String(bucketId),
 	})
 	assert.NoError(t, err, "Error checking S3 bucket location")
-	
 }
