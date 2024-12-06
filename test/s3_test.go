9ad71254847ab456fa41f8b4144566c283511095
RivuParth <bhowmick.partha1999@gmail.com>
2024-12-06 16:47:33 +0530

    Add GitHub Actions final terratest /////////

--- test/s3_test.go
+++ test/s3_test.go
@@ -10,32 +10,26 @@ import (
 func TestS3BucketModule(t *testing.T) {
 	t.Parallel()
 
-	// Define Terraform options
-	options := &terraform.Options{
-		// Path to your Terraform code
-		TerraformDir: "../modules/s3",
-		NoColor:      true, // Disable colored output for cleaner logs
+	terraformOptions := &terraform.Options{
+		TerraformDir: "../modules/s3", // Path to your Terraform code
+		NoColor:      true,
 	}
 
-	// Initialize and validate the Terraform configuration
-	terraform.Init(t, options)
-	terraform.Validate(t, options)
+	// Clean up resources after the test
+	defer terraform.Destroy(t, terraformOptions)
 
-	// Use the expected values for validation
-	expectedBucketName := "my-terratest-bucket-66666"
+	// Run Terraform init and apply
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Expected values
+	expectedBucketName := "my-terratest-bucket-666666"
 	expectedRegion := "us-east-1"
 
-	// Fetch outputs from Terraform
-	bucketName := terraform.Output(t, options, "bucket_name")
-	assert.NotEmpty(t, bucketName, "Bucket name should not be empty")
-	
-	bucketRegion := terraform.Output(t, options, "bucket_region")
-	assert.Equal(t, "us-east-1", bucketRegion, "Bucket region does not match expected value")
-	
-	bucketArn := terraform.Output(t, options, "bucket_arn")
-	assert.Contains(t, bucketArn, bucketName, "Bucket ARN should contain the bucket name")
-	
-	bucketId := terraform.Output(t, options, "bucket_id")
-	assert.NotEmpty(t, bucketId, "Bucket ID should not be empty")
-	
+	// Validate bucket name
+	actualBucketName := terraform.Output(t, terraformOptions, "bucket_name")
+	assert.Equal(t, expectedBucketName, actualBucketName, "Bucket name does not match expected value")
+
+	// Validate bucket region
+	actualRegion := terraform.Output(t, terraformOptions, "bucket_region")
+	assert.Equal(t, expectedRegion, actualRegion, "Bucket region does not match expected value")
 }
